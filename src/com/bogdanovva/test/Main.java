ab911bd39529049f7ebf56b66422c80e9862e157
BogdanovVA <BogdanovVA@rbtechnologies.ru>
2018-03-20 10:16:38 +0300

    Первый коммит.

---
+++ src/com/bogdanovva/test/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.bogdanovva.test;
+
+/**
+ * @author BogdanovVA
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!");
+	}
+
+}
